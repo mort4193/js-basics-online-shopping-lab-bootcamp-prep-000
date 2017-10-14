d45effdb8dee789ce9270763ee3f59c2403ab4d8
jonathan Morton <jmorton4193@gmail.com>
2017-10-14 06:47:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ function total() {var totalP=0
   }
 
 function removeFromCart(item) {
-    for (let i=0; i<cart.length; i+=0){
+    for (let i=0; i<cart.length; i++){
     if (cart[i].hasOwnProperty(item)){
     cart.splice(i, 1)
     return cart

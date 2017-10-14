2e3ec97acb82343dd698c8a84b5ee18b5f439d44
jonathan Morton <jmorton4193@gmail.com>
2017-10-14 06:58:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ return cart
 
 function placeOrder(cardNumber) {
 if (cardNumber===undefined){return "Sorry, we don\'t have a credit card on file for you\."}
-else { return `Your total cost is \$${total()}, which will be charged to the card ${cardNumber}\.`
- cart=[]
+else { return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}\.`
+ cart=[];
 }
 }
